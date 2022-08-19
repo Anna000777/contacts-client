deeeff18288d4e9de1fb697cfbc5af1bde305b9f
Anna <kazanok777@gmail.com>
2022-08-19 15:28:38 +0300

    modified functions

---
+++ src/check.js
@@ -0,0 +1,18 @@
+export function checkInput(input){
+  const {min, max} = input.dataset;
+  if (!input.value){
+    input.nextElementSibling.innerText = 'This field is required!';
+    return false;
+  }
+  if (input.value.length < min){
+    input.nextElementSibling.innerText = `This field has min value ${min}`;
+    return false;
+  }
+  if (input.value.length > max){
+    input.nextElementSibling.innerText = `This field has max value ${max}`;
+    return false;
+  }
+  input.nextElementSibling.innerText = '';
+  return true;
+
+}
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import {renderItem} from '../render.js';
+import {checkInput} from './check.js';
+
+export const form = document.querySelector('.form');
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+  form.addEventListener('change', ({target}) => {
+    checkInput(target);
+  });
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const {target} = event;
+    const isValid = [...form.elements]
+      .filter((input) => input.tagName === 'INPUT')
+      .every((input) => checkInput(input));
+
+    if (isValid){
+      const body = new FormData(target);
+      const data = await fetch('https://contacts-api-learn.herokuapp.com/api/contacts', {
+        method: 'POST',
+        body,
+      });
+      const contact = await data.json();
+      renderItem(contact);
+      form.reset();
+    }
+  });
+
+  const data = await fetch('https://contacts-api-learn.herokuapp.com/api/contacts');
+  const contacts = await data.json();
+  console.log(contacts);
+  Object.values(contacts).forEach(renderItem);
+
+})
